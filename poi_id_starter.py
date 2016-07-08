9730f07ad2660e15923546436d612379b37214df
kcbighuge <kcbighuge@gmail.com>
2016-07-08 17:43:22 -0500

    finalize report, update poi_id.py, create pkl files

--- poi_id_starter.py
+++ poi_id_starter.py
@@ -2,7 +2,6 @@
 
 import sys
 import pickle
-import pandas as pd
 sys.path.append("../tools/")
 
 from feature_format import featureFormat, targetFeatureSplit
@@ -11,7 +10,7 @@ from tester import dump_classifier_and_data
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
-features_list = ['poi','salary', 'exercised_stock_options', 'bonus', 'shared_receipt_with_poi', 'total_stock_value', 'deferred_income'] # You will need to use more features
+features_list = ['poi','salary'] # You will need to use more features
 
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
@@ -20,12 +19,7 @@ with open("final_project_dataset.pkl", "r") as data_file:
 ### Task 2: Remove outliers
 ### Task 3: Create new feature(s)
 ### Store to my_dataset for easy export below.
-clean_dict = dict(data_dict)
-del clean_dict['LOCKHART EUGENE E']
-del clean_dict['TOTAL']
-del clean_dict['LAY KENNETH L']
-
-my_dataset = clean_dict
+my_dataset = data_dict
 
 ### Extract features and labels from dataset for local testing
 data = featureFormat(my_dataset, features_list, sort_keys = True)
@@ -51,7 +45,7 @@ clf = GaussianNB()
 # Example starting point. Try investigating other evaluation techniques!
 from sklearn.cross_validation import train_test_split
 features_train, features_test, labels_train, labels_test = \
-    train_test_split(features, labels, stratify=labels, test_size=0.3, random_state=42)
+    train_test_split(features, labels, test_size=0.3, random_state=42)
 
 ### Task 6: Dump your classifier, dataset, and features_list so anyone can
 ### check your results. You do not need to change anything below, but make sure
